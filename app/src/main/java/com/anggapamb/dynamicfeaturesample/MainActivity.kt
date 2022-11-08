080c7d47cd3cbdce17c334cc3ed2dfa9accde43b
anggapamb <email_anda>
2022-11-08 15:57:21 +0700

    first commit

---
+++ app/src/main/java/com/anggapamb/dynamicfeaturesample/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.anggapamb.dynamicfeaturesample
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.play.core.splitinstall.SplitInstallManagerFactory
+import com.google.android.play.core.splitinstall.SplitInstallRequest
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnChat = findViewById<FloatingActionButton>(R.id.btn_chat)
+
+        btnChat.setOnClickListener {
+            try {
+                installChatModule()
+            } catch (e: Exception) {
+                Toast.makeText(this, "Module not found", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+
+    private fun installChatModule() {
+        val splitInstallManager = SplitInstallManagerFactory.create(this)
+        val moduleChat = "chat"
+        if (splitInstallManager.installedModules.contains(moduleChat)) {
+            moveToChatActivity()
+            Toast.makeText(this, "Open module", Toast.LENGTH_SHORT).show()
+        } else {
+            val request = SplitInstallRequest.newBuilder()
+                .addModule(moduleChat)
+                .build()
+            splitInstallManager.startInstall(request)
+                .addOnSuccessListener {
+                    Toast.makeText(this, "Success installing module", Toast.LENGTH_SHORT).show()
+                    moveToChatActivity()
+                }
+                .addOnFailureListener {
+                    Toast.makeText(this, "Error installing module", Toast.LENGTH_SHORT).show()
+                }
+        }
+    }
+
+    private fun moveToChatActivity() {
+        startActivity(Intent(this, Class.forName("com.anggapamb.chat.ChatActivity")))
+    }
+}
